07a92af2bd9569f222b390077f02711ed1ed0789
imgomez0127 <ianm0127@gmail.com>
2021-01-17 10:15:47 -0500

    Added stuff

--- backend/backend.py
+++ backend/backend.py
@@ -1,7 +1,10 @@
+import base64
 import json
+from random import shuffle
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
+import numpy as np
 import torch
 from models.lstm import get_weights, LSTM, filter_text
 
@@ -13,15 +16,30 @@ emotion_mapping = {i: emotion for i, emotion in enumerate(emotions)}
 
 with open("./models/mapping.json") as f:
     vocab_map = json.load(f)
-_, weights = get_weights("./models/data/glove.6B.50d.txt",
-                         vocab_map.keys(),
-                         add_zero=True)
+weights = np.zeros((len(vocab_map)+1, 50))
 
 weights = torch.from_numpy(weights)
 model = LSTM(weights, len(emotions), device=torch.device('cpu'), hidden_size=256)
 model.load("./models/model.pt", map_location=torch.device('cpu'))
 
-spotify_key = "INSERT KEY HERE"
+spotify_key = "764915ada4714643a561bb17286a9882:5f33e816137f43c2979824f69987c63e"
+key_bytes = spotify_key.encode("ascii")
+key_b64 = base64.b64encode(key_bytes)
+key_repr = key_b64.decode("ascii")
+auth = "Basic " + key_repr
+auth_key = ""
+def authorize_spotify():
+    headers = {
+        "Authorization": auth,
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    payload = [("grant_type","client_credentials")]
+    r = requests.post(
+        "https://accounts.spotify.com/api/token",
+        headers=headers,
+        data=payload
+    )
+    return r.json()["access_token"]
 
 @app.route("/", methods=["POST"])
 def sentiment():
@@ -36,7 +54,17 @@ def sentiment():
 @app.route("/get-song", methods=["POST"])
 def get_song():
     emotion = request.json['emotion']
-    return jsonify({"song-link":"INSERT LINK"})
+    url  = f'https://api.spotify.com/v1/search?q=${emotion} playlist&type=playlist&limit=5'
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + auth_key
+    }
+    r = requests.get(url, headers=headers)
+    items = r.json()['playlists']['items']
+    shuffle(items)
+    playlist = items[0]["external_urls"]["spotify"]
+    return jsonify({"song-link":playlist})
 
 if __name__ == "__main__":
+    auth_key = authorize_spotify()
     app.run(host="localhost", port=8000, debug=True)
--- frontend-2/scripts.js
+++ frontend-2/scripts.js
@@ -1,6 +1,8 @@
-//var text_input = document.getElementById("text-input");
-var input_button = document.getElementById("input-button");
+var text_input = document.getElementById("text-input");
+var input_form = document.getElementById("input-form");
 var emotion = null;
+var user_classes = ["chatbox1", "chatbubble-user"];
+var bot_classes = ["chatbox2", "chatbubble-bot"]
 let get_sentiment = async (text) => {
     url = "http://localhost:8000/"
     const response = await fetch(
@@ -30,20 +32,48 @@ let get_song = async (emotion) =>{
     return response.json();
 }
 
-input_button.addEventListener("click", async () => {
-    //let message  = text_input.value;
-    //text_input.value = ""
+input_form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    let message  = text_input.value;
+    text_input.value = ""
+    let tag = document.createElement("p");
+    tag.classList.add(...user_classes);
+    parent = document.getElementById("text")
+    tag.innerText = message;
+    console.log(tag)
+    parent.append(tag);
     if(!emotion){
-        response = await get_sentiment("naomi is awesome")
+        response = await get_sentiment(message)
             .then((data) => { return data });
         emotion = response["emotion"]
         console.log(emotion)
+        let feeling_tag = document.createElement("p");
+        feeling_tag.classList.add(...bot_classes);
+        feeling_tag.innerText = "Are you feeling " + emotion
+        console.log(feeling_tag)
+        parent.append(feeling_tag);
     }
-    else{
+    else if(message.toLowerCase() === "yes" || message.toLowerCase() === "ya"){
         response = await get_song(emotion)
             .then((data => {return data}));
         song = response["song-link"];
         console.log(song);
+        response_tag = document.createElement("p")
+        response_tag.classList.add(...bot_classes);
+        response_tag.innerText = "Try listening to this "
+        let link = document.createElement("a");
+        link.class = "unstyled-link"
+        link.href = song;
+        link.innerText = song;
+        response_tag.append(link)
+        parent.append(response_tag)
         emotion = null;
+    }
+    else{
+        response_tag = document.createElement("p");
+        response_tag.classList.add(...bot_classes);
+        response_tag.innerText = "Then how are you feeling"
+        parent.append(response_tag);
+
     }
 })
